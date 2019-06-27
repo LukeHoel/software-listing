581f72f4867ef012a3a15da15b1cafdb66c2d8c7
LukeHoel <hoel.luke@gmail.com>
2019-06-26 23:39:44 -0400

    re-add build

---
+++ build/precache-manifest.9d2f4c343994d79b21b6e2487fda8e21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c88eda05b8582446ae304b338a85ca",
+    "url": "/software-listing/index.html"
+  },
+  {
+    "revision": "3ab0c0491dd3a6414ac1",
+    "url": "/software-listing/static/css/main.adc5f080.chunk.css"
+  },
+  {
+    "revision": "779cad2b821d5f5f4c71",
+    "url": "/software-listing/static/js/2.234b16d4.chunk.js"
+  },
+  {
+    "revision": "3ab0c0491dd3a6414ac1",
+    "url": "/software-listing/static/js/main.06d2a3e0.chunk.js"
+  },
+  {
+    "revision": "742cbee26c69229bdc41",
+    "url": "/software-listing/static/js/runtime~main.b1bad945.js"
+  }
+]);
