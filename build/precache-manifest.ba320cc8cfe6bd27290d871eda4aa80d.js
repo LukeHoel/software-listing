032d7c1c482e99518cb5723838b06f0804f0c9ed
LukeHoel <hoel.luke@gmail.com>
2019-06-26 22:57:00 -0400

    added build folder

---
+++ build/precache-manifest.ba320cc8cfe6bd27290d871eda4aa80d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9226f5661efcd706b10bd20bd9866360",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8154084b86391c106f6e",
+    "url": "/static/css/main.adc5f080.chunk.css"
+  },
+  {
+    "revision": "779cad2b821d5f5f4c71",
+    "url": "/static/js/2.234b16d4.chunk.js"
+  },
+  {
+    "revision": "8154084b86391c106f6e",
+    "url": "/static/js/main.223e60b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
